63115a489def59ef8efc8781f3037e0950cff57a
DEMON-ITACHI <122287368+DEMON-ITACHI@users.noreply.github.com>
2023-01-10 19:17:55 +0530

    Create __init__.py

---
+++ Kushina*/Utils/__init__.py
@@ -0,0 +1,14 @@
+from . import (  # skipcq: PY-W2000
+    async_helper,
+    config,
+    converter,
+    db,
+    error,
+    misc,
+    system,
+    tg,
+    time,
+    types,
+)
+
+run_sync = async_helper.run_sync
